850d5ce179ee0a80166c73fcb62ff66b652adb73
Szilard <borsszili.03@gmail.com>
2024-02-21 18:13:36 +0100

    [SZAKDOGA] Rust actix web alapok

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod user;
 
-use actix_web::{App, HttpServer, Responder};
-use crate::user::user_class::{ test };
+use actix_web::{App, HttpServer};
+use crate::user::user_class::{get_services};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(test)
+            .service(get_services())
     })
     .bind(("127.0.0.1",8080))?
     .run()
--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,13 @@
 pub mod user_class {
-    use actix_web::{get, HttpResponse, Responder, Scope, web};
+    use actix_web::{get, HttpResponse, Responder, web};
     use actix_web::http::header::ContentType;
 
-    #[get("/user/list")]
+    pub fn get_services() -> actix_web::Scope {
+        web::scope("/user")
+            .service(test)
+    }
+
+    #[get("/list")]
     pub async fn test() -> impl Responder {
         HttpResponse::Ok()
             .content_type(ContentType::plaintext())
